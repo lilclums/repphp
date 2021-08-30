60f23467a020b58c9040ceca42596758b0219b91
clums <lslinares96@misena.edu.co>
2021-08-30 12:08:43 -0500

    funciones

--- inicio.php
+++ inicio.php
@@ -8,7 +8,28 @@
 </head>
 <body>
     <?php
-    echo 'iniciamos repositorio';
+    echo 'iniciamos repositorio <br>';
+    function suma(numeros){
+        $sum=0;
+        for ($i=0; $i <count($numeros) ; $i++) { 
+            $sum+=$numeros[$i];
+        }
+        return
+    };
+    $nums=array();
+    $nums1=[1,2,3,4,5,6,7,8,9];
+
+    for ($i=0; $i<20; $i++){
+        $nums[$i]=rand(0,100);
+    };
+
+    for ($i=0; $i <20 ; $i++) { 
+        echo $nums[$i].',';
+      //  $suma+=$nums[$i];
+    };
+echo 'la suma es = '.suma($nums).'<br>';
+echo 'la suma es = '.suma($nums1).'<br>';
+
     ?>
 </body>
 </html>
