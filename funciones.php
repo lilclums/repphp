d2678667b2567e40fb5b937fdc16a35da99e8512
lilclums <88675258+lilclums@users.noreply.github.com>
2021-09-01 10:57:30 -0500

    Add files via upload
    upload 3 functions form

---
+++ funciones.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>funcion 1</title>
+</head>
+<body>
+ 
+<form action="" method="POST" class="form"> 
+<p>numero 1</p>
+<input name="parcial1" type="number" step="any" class="input">
+<br>
+<p>Numero 1</p>
+<input name="parcial2" type="number" step="any" class="input">
+<br>
+<button type="submit" class="enviar" >Enviar</button>
+
+
+</form>
+</body>
+</html>
+<?php 
+
+?>
