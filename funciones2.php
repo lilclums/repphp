323ce16d76ba0ba8795252808d5f0f65625852f0
lilclums <88675258+lilclums@users.noreply.github.com>
2021-09-01 11:11:49 -0500

    changed to a better version

--- funciones2.php
+++ funciones2.php
@@ -4,16 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>funcion 1</title>
+    <title>funcion 2</title>
 </head>
 <body>
- 
+<h1>numero menor</h1>
 <form action="" method="POST" class="form"> 
 <p>numero 1</p>
-<input name="parcial1" type="number" step="any" class="input">
+<input name="numero1" type="number" step="any" class="input">
 <br>
-<p>Numero 1</p>
-<input name="parcial2" type="number" step="any" class="input">
+<p>Numero 2</p>
+<input name="numero2" type="number" step="any" class="input">
 <br>
 <button type="submit" class="enviar" >Enviar</button>
 
@@ -22,5 +22,16 @@
 </body>
 </html>
 <?php 
+    $num = $_POST["numero1"];
+    $num2 = $_POST["numero2"];
 
+    function NumeroMenor($arrayNumero)
+    {
+      $menor=0;
+      foreach ($arrayNumero as $num2) {
+        if ($num<$num2)
+         $menor = $num;
+      }
+      return $menormayor;
+    }
 ?>
